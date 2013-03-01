281ad57c1eac6251cd88a7806be0d7a9361df9e8
Vadim <6600gm@gmail.com>
2013-03-02 01:43:33 +0200

    code init

---
+++ ExpressionVisitor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BTR.Core.Linq
+{
+	public static class ExpressionExtensions
+	{
+		public static Expression Visit<T>(
+			this Expression exp,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true ) where T : Expression
+		{
+			return ExpressionVisitor<T>.Visit( exp, visitor, visitReplacement );
+		}
+
+		public static TExp Visit<T, TExp>(
+			this TExp exp,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true )
+			where T : Expression
+			where TExp : Expression
+		{
+			return (TExp)ExpressionVisitor<T>.Visit( exp, visitor, visitReplacement );
+		}
+
+		public static Expression<TDelegate> Visit<T, TDelegate>(
+			this Expression<TDelegate> exp,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true ) where T : Expression
+		{
+			return ExpressionVisitor<T>.Visit<TDelegate>( exp, visitor, visitReplacement );
+		}
+
+		public static IQueryable<TSource> Visit<T, TSource>(
+			this IQueryable<TSource> source,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true ) where T : Expression
+		{
+			return source.Provider.CreateQuery<TSource>( ExpressionVisitor<T>.Visit( source.Expression, visitor, visitReplacement ) );
+		}
+	}
+
+	/// <summary>
+	/// This class visits every Parameter expression in an expression tree and calls a delegate
+	/// to optionally replace the parameter.  This is useful where two expression trees need to
+	/// be merged (and they don't share the same ParameterExpressions).
+	/// </summary>
+	public class ExpressionVisitor<T> : ExpressionVisitor where T : Expression
+	{
+		private Func<T, Expression> visitor;
+		private bool visitReplacement;
+
+		public ExpressionVisitor( Func<T, Expression> visitor, bool visitReplacement = true )
+		{
+			this.visitor = visitor;
+			this.visitReplacement = visitReplacement;
+		}
+
+		public static Expression Visit(
+			Expression exp,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true )
+		{
+			return new ExpressionVisitor<T>( visitor, visitReplacement ).Visit( exp );
+		}
+
+		public static Expression<TDelegate> Visit<TDelegate>(
+			Expression<TDelegate> exp,
+			Func<T, Expression> visitor,
+			bool visitReplacement = true )
+		{
+			return (Expression<TDelegate>)new ExpressionVisitor<T>( visitor, visitReplacement ).Visit( exp );
+		}
+
+		public override Expression Visit( Expression exp )
+		{
+			var result = ( exp is T && visitor != null ) ? visitor( (T)exp ) : exp;
+
+			return ( result != exp && !visitReplacement ) ? result : base.Visit( result );
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "LinqPost" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Benefit Technology Resource" )]
+[assembly: AssemblyProduct( "LinqPost" )]
+[assembly: AssemblyCopyright( "Copyright © Benefit Technology Resource 2008" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "8ba2b4d7-556a-4504-b794-e536fa713e58" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
